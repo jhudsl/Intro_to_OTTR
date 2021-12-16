549bf230b61524cb6cfa8703b202e1786564b02b
Candace Savonen <cansav09@gmail.com>
2021-12-16 15:15:06 -0500

    Some progress

--- scripts/file-handling.R
+++ scripts/file-handling.R
@@ -7,10 +7,16 @@ folder_name <- "DaSL_Course_Template_Bookdown.wiki"
 # Find .git root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
+bookdown_file <- file.path(root_dir, "_bookdown.yml")
+
+# Read bookdown
+bookdown <- yaml::read_yaml(bookdown_file)
+
 # Get sidebar file names
 sidebar <- readLines(file.path(folder_name, "_Sidebar.md"))
 sidebar <- grep("#|\\[", sidebar, value = TRUE)
-names(sidebar) <- sidebar
+
+
 
 # Trim off lines
 sidebar <- stringr::word(sidebar, sep = "wiki/", 2)
@@ -19,3 +25,18 @@ sidebar <- gsub(")", "", sidebar)
 # filenames
 filenames <- sidebar[!is.na(sidebar)]
 
+# Copy over files
+file.copy(from = file.path(folder_name, paste0(filenames, ".md")), 
+          to =  paste0(filenames, ".Rmd"))
+
+filenames <- paste0(filenames, ".Rmd")
+filenames
+
+bookdown$rmd_files <- c(
+  "index.Rmd",
+  filenames,
+  "About.Rmd",
+  "References.Rmd"
+)
+
+yaml::write_yaml(bookdown, file = bookdown_file)
